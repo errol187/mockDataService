8beebdd47d6012ad93ee65327be55de7331bc739
Errol <errol187@hotmail.com>
2018-05-14 23:52:24 +0100

    Output values per index and not per object

--- mockService.js
+++ mockService.js
@@ -1,30 +1,59 @@
 "use strict";
 
-var indx = 1, delay = 2000, limit = 20;
+var indx = 2, delay = 2000, limit = 20, timeStamp = {};
+var key = "key", value = "value";
 
-// TODO: Randomise results to mimic a service that is updated regularly
-var fnc = function (){
-    var obj = {}, sDate, indx = 0, data;
-    obj[new Date()] = 1;
+// assigning the date to a key:value pairing defaults to type 'string'
+timeStamp[new Date()] = 1;
+var jObj = {"test":"obj"}, jArray = ["testArray"];
 
-    // TODO: Return dynamic results
-    while (indx <= limit) {
-        console.log(obj, indx);
-        ++indx;
+var serviceObj = () => {
+
+    // for (var i = 0; i < 20 ; i++ ) {
+    while ( indx < 20) {
+        console.log("valueStart");
+        jArray.push(value+indx);
+        console.log(jArray);
+        console.log("valueEnd");
+
+        for (key in jArray){
+            // console.log(key);
+            console.log("start");
+            console.log(jArray[key]);
+            console.log("end");
+        }
+    ++indx;
     }
-};
 
-var clearInt = function (fnc) {
-    if (indx === limit) {
-        console.log("You have reached your limit");
-        clearInterval(fnc());
-    }
-};
+}
 
-var mockService = (fnc, delay) => {
-    setInterval(fnc, delay);
-};
+serviceObj();
 
-mockService(fnc, delay);
 
-clearInt();
+//
+// // TODO: Randomise results to mimic a service that is updated regularly
+// var fnc = function (){
+//     var sDate, indx = 0, data;
+//
+//     // TODO: Return dynamic results
+//     while (indx <= limit) {
+//         console.log(timeStamp, indx);
+//         ++indx;
+//     }
+// };
+//
+// // Clear the interval that has been set
+// var clearInt = function (fnc) {
+//     if (indx === limit) {
+//         console.log("You have reached your limit");
+//         clearInterval(fnc());
+//     }
+// };
+//
+// var mockService = (fnc, delay) => {
+//     setInterval(fnc, delay);
+// };
+//
+// mockService(fnc, delay);
+//
+// clearInt();
